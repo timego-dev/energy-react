41df3ce6bf32d1195343495e348b83c659a2d536
timego-dev <timego.market@gmail.com>
2023-11-20 22:46:44 +0100

    prepare assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { ReactComponent as HomeIcon } from "./assets/home.svg";
+import { ReactComponent as SunIcon } from "./assets/sun.svg";
+import { ReactComponent as CloudIcon } from "./assets/cloud.svg";
+import { ReactComponent as BatteryIcon } from "./assets/battery.svg";
+import { ReactComponent as GridIcon } from "./assets/grid.svg";
+import { ReactComponent as ArrowIcon } from "./assets/arrow.svg";
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:3200/energy.php")
+      .then((res: Response) => res.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <SunIcon className="sun" />
+        <ArrowIcon className="grid-arrow" />
+        <ArrowIcon className="battery-arrow" />
+      <CloudIcon className="cloud" />
+      <BatteryIcon className="battery" />
+      <GridIcon className="grid" />
+      <HomeIcon className="home" />
     </div>
   );
 }
